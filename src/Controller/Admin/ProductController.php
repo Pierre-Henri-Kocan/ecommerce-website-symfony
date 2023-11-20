4e19e659689a2f274123bd30a5e31189c7d29a20
ph.kocan <ph.kocan@gmail.com>
2023-11-20 16:59:47 +0100

    Product form for create and update created

--- src/Controller/Admin/ProductController.php
+++ src/Controller/Admin/ProductController.php
@@ -3,9 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Product;
+use App\Form\ProductFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/admin/product', name: 'admin_product_')]
 class ProductController extends AbstractController
@@ -17,18 +21,83 @@ class ProductController extends AbstractController
     }
 
     #[Route('/create', name: 'create')]
-    public function create(): Response
+    public function create(Request $request, EntityManagerInterface $em, SluggerInterface $slugger): Response
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
-        return $this->render('admin/product/index.html.twig');
+
+        // On créé un nouveau produit
+        $product = new Product();
+
+        // On créé le formulaire
+        $productForm = $this->createForm(ProductFormType::class, $product);
+
+        // On traite la requête du formulaire
+        $productForm->handleRequest($request);
+       
+        // On vérifie que le formulaire est soumis et valdie
+        if ($productForm->isSubmitted() && $productForm->isValid()) {
+            // On génère le slug
+            $slug = $slugger->slug(strtolower($product->getName()));
+            $product->setSlug($slug);
+
+            // On arrondit le prix
+            $price = $product->getPrice() * 100;
+            $product->setPrice($price);
+
+            // On stocke en BDD
+            $em->persist($product);
+            $em->flush();
+
+            $this->addFlash('success', 'Produit ajouté avec succès');
+
+            // On redirige
+            return $this->redirectToRoute('admin_product_index');
+        }
+
+        return $this->render('admin/product/create.html.twig', [
+            'productForm' => $productForm->createView(),
+        ]);
     }
 
     #[Route('/update/{id}', name: 'update')]
-    public function update(Product $product): Response
+    public function update(Product $product, Request $request, EntityManagerInterface $em, SluggerInterface $slugger): Response
     {
         // On vérifie si l'utilisateur peut éditer avec le Voter
         $this->denyAccessUnlessGranted('PRODUCT_UPDATE', $product);
-        return $this->render('admin/product/index.html.twig');
+
+        // On divise le prix par 100
+        $price = $product->getPrice() / 100;
+        $product->setPrice($price);
+
+        // On créé le formulaire
+        $productForm = $this->createForm(ProductFormType::class, $product);
+
+        // On traite la requête du formulaire
+        $productForm->handleRequest($request);
+       
+        // On vérifie que le formulaire est soumis et valdie
+        if ($productForm->isSubmitted() && $productForm->isValid()) {
+            // On génère le slug
+            $slug = $slugger->slug(strtolower($product->getName()));
+            $product->setSlug($slug);
+
+            // On arrondit le prix
+            $price = $product->getPrice() * 100;
+            $product->setPrice($price);
+
+            // On stocke en BDD
+            $em->persist($product);
+            $em->flush();
+
+            $this->addFlash('success', 'Produit modifié avec succès');
+
+            // On redirige
+            return $this->redirectToRoute('admin_product_index');
+        }
+
+        return $this->render('admin/product/update.html.twig', [
+            'productForm' => $productForm->createView(),
+        ]);
     }
 
     #[Route('/delete/{id}', name: 'delete')]
---
+++ src/Form/ProductFormType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Product;
+use App\Repository\CategoryRepository;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ProductFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description'
+            ])
+            ->add('price', IntegerType::class, [
+                'label' => 'Prix'
+            ])
+            ->add('stock', IntegerType::class, [
+                'label' => 'Unités en stock'
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'label' => 'Catégorie',
+                'group_by' => 'parent.name',
+                'query_builder' => function(CategoryRepository $categoryRepository)
+                {
+                    return $categoryRepository->createQueryBuilder('c')
+                    ->where('c.parent IS NOT NULL')
+                    ->orderBy('c.name', 'ASC');
+                }
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
